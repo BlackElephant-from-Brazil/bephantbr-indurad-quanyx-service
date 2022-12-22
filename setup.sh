32cb0e236d18c540202c9a95ea8a51f7ccb9b36f
Guilherme Sartori <gui.sartori96@gmail.com>
2022-12-22 16:20:11 -0300

    fix(setup): setup must install ros correctlly

--- setup.sh
+++ setup.sh
@@ -10,13 +10,23 @@ echo "FINISH: Install OMEGA ASH"
 
 # Install all Packages to run ROS 
 echo "BEGIN: Install all Packages to run ROS"
+export ROS_PYTHON_VERSION=3
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+# apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt update -y
-apt-key adv --keyserver hkps://keyserver.ubuntu.com --refresh-keys
-apt install cmake python-rosinstall python-rosinstall-generator python-wstool build-essential ros-melodic-desktop-full ros-melodic-pcl-ros -y
-apt-cache policy libopencv-dev
-apt-get install libopencv-dev=3.2.0+dfsg-4ubuntu0.1 -V -y
+apt install ros-melodic-desktop-full -y
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+apt install python-rosdep
+rosdep init
+rosdep update -y
+# apt-key adv --keyserver hkps://keyserver.ubuntu.com --refresh-keys
+# apt install cmake python-rosinstall python-rosinstall-generator python-wstool build-essential ros-melodic-desktop-full ros-melodic-pcl-ros -y
+# apt-cache policy libopencv-dev
+# apt-get install libopencv-dev=3.2.0+dfsg-4ubuntu0.1 -V -y
 echo "FINISH: Install all Packages to run ROS"
 
 # Setup catkin to 3.11
@@ -33,20 +43,18 @@ echo "FINISH: Setup catkin to 3.11"
 
 # Configurating environment
 echo "BEGIN: Configurating environment"
-source /opt/ros/melodic/setup.bash
 apt-get install python-rosdep python-catkin-tools python-rospkg -y
-rosdep init
-rosdep update -y
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
 catkin init
 cd src
 cp -r /usr/bephantbr-indurad-quanyx-service/ROS/* .
 cd ..
-rosdep install --from-paths src --ignore-src -r -y
 catkin config --extend /opt/ros/melodic
+cp /usr/bephantbr-indurad-quanyx-service/cv_bridgeConfig.cmake /opt/ros/melodic/share/cv_bridge/cmake/
 catkin build
 source devel/setup.bash
+rosdep install --from-paths src --ignore-src -r -y
 echo "FINISH: Configurating environment"
 
 # Set git configuration
