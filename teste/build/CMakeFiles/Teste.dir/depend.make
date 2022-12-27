f7b6ffc1fadb140803edf5dfd15570492afe5427
Guilherme Sartori <gui.sartori96@gmail.com>
2022-12-27 13:28:58 -0300

    feat(io): remove teste folder again for the third time

--- teste/build/CMakeFiles/Teste.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Teste.
-# This may be replaced when dependencies are built.
--- teste/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char const *argv[])
-{
-    system("pkill avahi");
-    system("avahi-autoipd -D --force-bind --no-chroot eth0");
-    return 0;
-}
